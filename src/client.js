352263a16d984ce1c273f05982cde5bb0c02175a
MyeonghunIsDaBest <jordanbunuan@gmail.com>
2026-01-14 23:20:41 +0800

    fixed current issue with white page\blank page therefor web deployment and ui design is not yet available

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+    "https://ltsvhaxskqwqawljbhef.supabase.co",
+    "sb_publishable_W5GgTsX8vmTEOIo2zs0iiw_iomsPUNK"
+)
