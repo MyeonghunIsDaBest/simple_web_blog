8cc9d130f43118ab10e5d4d99de85a3c21274d51
MyeonghunIsDaBest <jordanbunuan@gmail.com>
2026-01-15 03:55:11 +0800

    removed client.js as it's unnecessary and updated the sb api key

--- src/client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-    "https://ltsvhaxskqwqawljbhef.supabase.co",
-    "sb_publishable_W5GgTsX8vmTEOIo2zs0iiw_iomsPUNK"
-)
